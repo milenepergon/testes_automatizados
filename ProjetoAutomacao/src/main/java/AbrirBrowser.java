4ae5d942869b1a691f3f6255ee63ff65b0d7b1ee
Milene Pergon <milenepergon@gmail.com>
2022-12-29 22:03:08 -0300

    deploy

--- ProjetoAutomacao/src/main/java/AbrirBrowser.java
+++ ProjetoAutomacao/src/main/java/AbrirBrowser.java
@@ -1,12 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AbrirBrowser {
-	public static void main(String[] args) {
+	
+	@Test
+	public void teste() {
 		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
 		driver.get("http://google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		driver. quit();
 		
+
 	}
 
 }
